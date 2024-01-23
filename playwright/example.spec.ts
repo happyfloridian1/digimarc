03560bbafd6d38a313d081279ef6876f955e6e63
Neubert <jneubert@dmrc-a005003.lan>
2024-01-23 12:42:22 -0700

    first push and test broken for screen

--- playwright/example.spec.ts
+++ playwright/example.spec.ts
@@ -4,4 +4,5 @@ test('can add an item', async ({ page }) => {
   await page.goto('/');
   await page.getByPlaceholder('What\'s up ?').fill('pickup dog');
   await page.getByPlaceholder('What\'s up ?').press('Enter');
+  await expect(page.locator('div').filter({ hasText: /^pick up dog$/ }).nth(2)).toBeVisible()
 });
