44ff3dfe72e4193ab87ba8d457f4ba391d151ada
happyfloridian1 <monicutz20@gmail.com>
2024-01-25 12:39:57 -0500

    fixed locators to pass the test

--- playwright/example.spec.ts
+++ playwright/example.spec.ts
@@ -1,8 +1,13 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('can add an item', async ({ page }) => {
-  await page.goto('/');
-  await page.getByPlaceholder('What\'s up ?').fill('pickup dog');
-  await page.getByPlaceholder('What\'s up ?').press('Enter');
-  await expect(page.locator('div').filter({ hasText: /^pick up dog$/ }).nth(2)).toBeVisible()
+test("can add an item", async ({ page }) => {
+  await page.goto("/");
+  await page.getByPlaceholder("What's up ? ...").fill("pickup dog");
+  await page.getByPlaceholder("What's up ? ...").press("Enter");
+  await expect(page.getByText("pickup dog")).toBeVisible();
 });
+// page
+//  .locator("span")
+// .filter({ hasText: /^pick up dog$/ })
+//  .nth(2)
+// ).toBeVisible();
